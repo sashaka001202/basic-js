47e73837e3322d6af7addd409d3ca50f9caf8c40
alex <sashaka00120@mail.ru>
2022-05-01 09:57:43 +0300

    feat: add simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  Arr:[],
+  
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      return this.Arr.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+
+      
+      if (value===null){
+          value ='null'
+      }
+
+      
+       this.Arr.push(value)
+       return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+
+      if (!Number(position)||position<1||position>this.Arr.length){
+          this.Arr=[]
+          throw new Error('You can\'t remove incorrect link!')
+      }
+
+      this.Arr.splice(position-1,1)
+      return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      this.Arr.reverse()
+      return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let newArr=[];
+      if (this.Arr.length==1){
+          newArr='( '+this.Arr[0]+' )'
+      }
+      else {
+          let first ='( '+this.Arr[0]
+          let last =this.Arr[this.Arr.length-1]+' )'
+          
+          
+          this.Arr.splice(0,1,first)
+          this.Arr.splice(this.Arr.length-1,1,last )
+  
+          newArr=this.Arr.join(' )~~( ')
+      }
+
+      
+      this.Arr=[]
+      return newArr
   }
 };
 
